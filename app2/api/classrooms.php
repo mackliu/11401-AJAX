b6a2abdbee209ba1e90a0a266548a7cf16e3f9de
mackliu <macklun@gmail.com>
2025-07-07 11:03:06 +0800

    在班級按鈕上增加班級代碼後,使用ajax來取得全班同學資料

--- app2/api/classrooms.php
+++ app2/api/classrooms.php
@@ -16,7 +16,7 @@ $ref=[
     '110'=>'一年十班',
 ];
 foreach($classrooms as $key => $classroom){
-    $classrooms[$key]['classroom']=$ref[$classroom['classroom']];
+    $classrooms[$key]['name']=$ref[$classroom['classroom']];
 }
 
 header('Content-Type: application/json;');
